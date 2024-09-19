1037b04298d5e2455d5b7a27795a8d62aa44a5fb
safnaz <sapnazfathimath543@gmail.com>
2024-09-19 15:20:49 +0530

    ABCS

---
+++ script.js
@@ -0,0 +1,7 @@
+// Toggle navigation menu on mobile
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+navToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
